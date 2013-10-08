f126e2c29d59f026de6e053504c57d59c6d728d9
Sateesh N <sateesh.naramsetti@gmail.com>
2013-10-08 19:46:04 +0530

    first mock checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MockHack::Application.config.secret_token = '435eff5d78a3e9127a499fe83cc17998d1b71254cd1045345fe78733ca2a47abf2f278d8a6911475b7c8b5f4b709e3def39c79110742233d5c21da7c874f3e35'
