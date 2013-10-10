f950ca6401135e342522d20e48b1d794188a1fdf
Janani Vijayan <janani29590@gmail.com>
2013-10-10 20:34:32 +0530

    Model Recreation
    Model Recreation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 MockHack::Application.routes.draw do
+	
+	scope '/admin' do
+		resources :products
+	end
+	
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -26,6 +31,7 @@ MockHack::Application.routes.draw do
   #   end
 
   # Sample resource route with sub-resources:
+  
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
@@ -55,4 +61,5 @@ MockHack::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+    
 end
