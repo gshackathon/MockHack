b00492d773b0bf22b118adcddf258060173da5d4
Barath <barath0811@gmail.com>
2013-10-12 15:11:40 +0530

    Adding auth to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   helper_method :sort_column, :sort_direction
+  before_filter :authenticate_user!, :except => [:list]
 
   # GET /products
   # GET /products.json
